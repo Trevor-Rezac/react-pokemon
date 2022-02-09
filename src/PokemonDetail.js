25b0fea6439447aaa1bb902180e6cb4949f7f491
TrevorRezac <trev.rezac@gmail.com>
2022-02-09 13:24:57 -0800

    added more details to PokemonDetail

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { getSinglePokemon } from './services/fetch-utils';
+import { Link } from 'react-router-dom';
 
 
 export default function PokemonDetail() {
@@ -19,13 +20,19 @@ export default function PokemonDetail() {
 
   }, [params.id]);
 
-  console.log('||', pokemon);
+  // console.log('||', pokemon);
 
   return (
-    <>
+    <> <Link to='/'>Home</Link>
       <div>PokemonDetail</div>
       <h2> {pokemon.pokemon} 
       </h2>
+      <p>HP: {pokemon.hp}</p>
+      <img src={`${pokemon.url_image}`}></img>
+      <p>Type1: {pokemon.type_1}</p>
+      <p>Type2: {pokemon.type_2}</p>
+      <p>Ability1: {pokemon.ability_1}</p>
+      <p>Attack: {pokemon.attack}</p>
     </>
   );
 }
