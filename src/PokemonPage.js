f92eaf1fc42679306022a2701b590934af22e1ed
TrevorRezac <trev.rezac@gmail.com>
2022-02-09 14:09:53 -0800

    search seems to work

--- src/PokemonPage.js
+++ src/PokemonPage.js
@@ -4,6 +4,7 @@ import { getAllPokemon } from './services/fetch-utils';
 
 
 export default function PokemonPage() {
+  const [searchQuery, setSearchQuery] = useState('');
   const [pokemon, setPokemon] = useState([]);
   const [page, setPage] = useState(1);
   const perPage = 50;
@@ -14,7 +15,7 @@ export default function PokemonPage() {
     async function fetchPokemon() {
       const from = perPage * (page - 1);
       const to = page * perPage;
-      const pokemon = await getAllPokemon(from, to);
+      const pokemon = await getAllPokemon(from, to, searchQuery);
 
 
       setPokemon(pokemon);
@@ -22,16 +23,19 @@ export default function PokemonPage() {
 
     fetchPokemon();
 
-  }, [page]);
+  }, [page, searchQuery]);
 
 
   return (
     <>
-      <div>PokemonPage {page}</div>
+      <h2>PokemonPage {page}</h2>
       <div className='btn-container'>
         <button onClick={() => {setPage(page - 1);}} disabled={page === 1}>Previous Page</button>
         <button onClick={() => {setPage(page + 1);}} disabled={pokemon.length < perPage}>Next Page</button>
       </div>
+      <h4>Search for Pokemon</h4>
+      <input 
+        onChange={(e) => setSearchQuery(e.target.value)}></input>
       <PokemonList pokemon={pokemon}/>
     </>
   );
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,12 +1,23 @@
 import { checkError, client } from './client.js';
 
-export async function getAllPokemon(from = 0, to = 50) {
-  const response = await client
-    .from('pokemon')
-    .select()
-    .range(from, to);
-
-  return checkError(response);
+export async function getAllPokemon(from = 0, to = 50, searchQuery) {
+  
+  if (!searchQuery) {
+    const response = await client
+      .from('pokemon')
+      .select()
+      .range(from, to);
+  
+    return checkError(response);
+  } else {
+    const response = await client
+      .from('pokemon')
+      .select()
+      .range(from, to)
+      .ilike('pokemon', `%${searchQuery}%`);
+    
+    return checkError(response);
+  }
 }
 
 export async function getSinglePokemon(id) {
