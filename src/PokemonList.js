f002d17107a8c1f42bf35e3010a38690c871116e
TrevorRezac <trev.rezac@gmail.com>
2022-02-10 17:45:09 -0800

    attempted class component refactor

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Pokemon from './Pokemon';
 
-export default function PokemonList({ pokemon }) {
+// export default function PokemonList({ pokemon }) {
   
-  return (
-    <>
-      <div className='pokemon-list'>
-        {pokemon.map((pokemon, i) => <Pokemon key={`${pokemon}-${i}`} pokemon={pokemon} />)}
-      </div>
-    </>
-  );
+//   return (
+//     <>
+//       <div className='pokemon-list'>
+//         {pokemon.map((pokemon, i) => <Pokemon key={`${pokemon}-${i}`} pokemon={pokemon} />)}
+//       </div>
+//     </>
+//   );
+// }
+
+
+
+export default class PokemonList extends Component {
+  render() {
+    const { pokemon } = this.props;
+    
+    return (
+      <>
+        <div className='pokemon-list'>
+          {pokemon.map((pokemon, i) => <Pokemon key={`${pokemon}-${i}`} pokemon={pokemon} />)}
+        </div>
+      </>
+    );
+  }
 }
